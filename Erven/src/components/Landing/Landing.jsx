a89269f6b376286689a2c19c2f6d0a2256e4c798
gerssss97 <gerlucero1997@gmail.com>
2024-11-28 23:02:35 -0300

    se arregla footer y flexbox del contenido

--- Erven/src/components/Landing/Landing.jsx
+++ Erven/src/components/Landing/Landing.jsx
@@ -1,52 +1,89 @@
-import React from 'react';
-import './Landing.css';
+import React from "react";
+import "./Landing.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Carrusel from './Carrusel';
-import frame from './Frame 1.jpg';
-import imagen from './trabajandoGrupo 1.png'
-import triangulo from './Polygon 1.svg'
-import Footer from '../Footer/Footer';
-
-import '../Footer/Footer.css';
-import instagram from '../Footer/instagram.svg';
-import facebook from '../Footer/facebook.svg'
+import Carrusel from "./Carrusel";
+import imagen from "./trabajandoGrupo 1.png";
+import triangulo from "./Polygon 1.svg";
 
 
-
-const Landing = ()=> {
+const Landing = () => {
   return (
-  <><div className="container-TODO">
-  <div class="trabajandogrupo-1-parent">
-    <img className="trabajandogrupo-1-icon" src={imagen}></img>
-    <div className="rectangulo-azul"></div>
-    <div className="slogan">SLOGAN</div>
-    <img className="triangulo" src={triangulo}></img>
-    <div className="rectangulo-blanco"></div>
-    <div className="rectangulo-gris"></div>
-  </div>
+    <>
+      <div >
+        <div class="trabajandogrupo-1-parent">
+          <img className="trabajandogrupo-1-icon" src={imagen}></img>
+          <div className="rectangulo-azul"></div>
+          <div className="slogan">SLOGAN</div>
+          <img className="triangulo" src={triangulo}></img>
+          <div className="rectangulo-blanco"></div>
+          <div className="rectangulo-gris"></div>
+        </div>
 
-  <div className='texto-central'> <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, 
-    nobis ex, ad tenetur libero saepe voluptatum est alias incidunt, itaque dignissimos! Enim
-     maxime qui laudantium ut, provident cumque delectus temporibus!</p></div>
-  <Carrusel></Carrusel>
-  <div className='texto-central'> <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, 
-    nobis ex, ad tenetur libero saepe voluptatum est alias incidunt, itaque dignissimos! Enim
-     maxime qui laudantium ut, provident cumque delectus temporibus!</p></div>
-  </div>
+        <div className="texto-central">
+          {" "}
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae,
+            nobis ex, ad tenetur libero saepe voluptatum est alias incidunt,
+            itaque dignissimos! Enim maxime qui laudantium ut, provident cumque
+            delectus temporibus!
+          </p>
+        </div>
 
-  <div>
-  <table className="mi-tabla">
-        <tr>
-          <td className="column">
-            <ul className="lista">
-            
-            </ul>
-          </td>
-        </tr>
-    </table>
-  </div>
-  </> 
+        <Carrusel></Carrusel>
+
+        <div className="texto-central">
+          {" "}
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae,
+            nobis ex, ad tenetur libero saepe voluptatum est alias incidunt,
+            itaque dignissimos! Enim maxime qui laudantium ut, provident cumque
+            delectus temporibus!
+          </p>
+        </div>
+      </div>
+        <div className="texto-central">
+          {" "}
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae,
+            nobis ex, ad tenetur libero saepe voluptatum est alias incidunt,
+            itaque dignissimos! Enim maxime qui laudantium ut, provident cumque
+            delectus temporibus!
+          </p>
+        </div><div className="texto-central">
+          {" "}
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae,
+            nobis ex, ad tenetur libero saepe voluptatum est alias incidunt,
+            itaque dignissimos! Enim maxime qui laudantium ut, provident cumque
+            delectus temporibus!
+          </p>
+        </div><div className="texto-central">
+          {" "}
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae,
+            nobis ex, ad tenetur libero saepe voluptatum est alias incidunt,
+            itaque dignissimos! Enim maxime qui laudantium ut, provident cumque
+            delectus temporibus!
+          </p>
+        </div><div className="texto-central">
+          {" "}
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae,
+            nobis ex, ad tenetur libero saepe voluptatum est alias incidunt,
+            itaque dignissimos! Enim maxime qui laudantium ut, provident cumque
+            delectus temporibus!
+          </p>
+        </div><div className="texto-central">
+          {" "}
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae,
+            nobis ex, ad tenetur libero saepe voluptatum est alias incidunt,
+            itaque dignissimos! Enim maxime qui laudantium ut, provident cumque
+            delectus temporibus!
+          </p>
+        </div>
+    </>
   );
 };
 
--- Erven/src/components/contenedor.jsx
+++ Erven/src/components/contenedor.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import Header from './header/Header'
 import Footer from './Footer/Footer'
 import { Outlet } from 'react-router'
+import './contenedor.css'
 
 
 const Contenedor = () => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', width:'100vw', position: 'relative'}}>
+    <div className='all'>
       <Header />
       <Outlet />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
